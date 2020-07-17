e451374b4756566e763355bb7592524909481ad5
yyj8209 <yyj8209@163.com>
2020-07-17 08:15:51 +0800

    first commit

---
+++ sample/app/src/main/java/com/dji/sdk/sample/demo/MApplication.java
@@ -0,0 +1,28 @@
+package com.dji.sdk.sample.demo;
+
+import android.app.Application;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.IntentFilter;
+
+import com.secneo.sdk.Helper;
+
+import static com.dji.sdk.sample.demo.DJIConnectionControlActivity.ACCESSORY_ATTACHED;
+
+public class MApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        BroadcastReceiver br = new OnDJIUSBAttachedReceiver();
+        IntentFilter filter = new IntentFilter();
+        filter.addAction(ACCESSORY_ATTACHED);
+        registerReceiver(br, filter);
+    }
+
+    @Override
+    protected void attachBaseContext(Context paramContext) {
+        super.attachBaseContext(paramContext);
+        Helper.install(MApplication.this);
+    }
+}
