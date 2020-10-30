50022b98808dd4eda2b790237714275c48cbf498
yyj8209 <yyj8209@163.com>
2020-10-30 20:06:19 +0800

    尝试地图与图像位置交换，未成功，暂时不改进

--- sample/app/src/main/java/com/wayful/speechflying/CompleteWidgetActivity.java
+++ sample/app/src/main/java/com/wayful/speechflying/CompleteWidgetActivity.java
@@ -44,6 +44,8 @@ import com.wayful.speechflying.util.FucUtil;
 import com.wayful.speechflying.util.XmlParser;
 import com.dji.mapkit.core.maps.DJIMap;
 import com.dji.mapkit.core.models.DJILatLng;
+
+import androidx.fragment.app.FragmentActivity;
 import dji.common.error.DJIError;
 import dji.common.flightcontroller.FlightOrientationMode;
 import dji.common.flightcontroller.simulator.InitializationData;
@@ -65,7 +67,7 @@ import dji.ux.widget.MapWidget;
 /**
  * Activity that shows all the UI elements together
  */
-public class CompleteWidgetActivity extends Activity {
+public class CompleteWidgetActivity extends FragmentActivity {
 
     private static final String TAG = "CompleteWidgetActivity";
     private static final String TAG_DEBUG = "debug";
@@ -73,9 +75,6 @@ public class CompleteWidgetActivity extends Activity {
     private MapWidget mapWidget;
     private ViewGroup parentView;
     private FPVWidget fpvWidget;
-    private FPVWidget secondaryFPVWidget;
-    private RelativeLayout primaryVideoView;
-    private FrameLayout secondaryVideoView;
     private boolean isMapMini = true;
 
     private int height;
@@ -134,13 +133,13 @@ public class CompleteWidgetActivity extends Activity {
 
         parentView = (ViewGroup) findViewById(R.id.root_view);
 
-//        fpvWidget = findViewById(R.id.fpv_widget);
-//        fpvWidget.setOnClickListener(new View.OnClickListener() {
+//        fpvWidget = findViewById( R.id.fpv_widget );
+//        fpvWidget.setOnClickListener( new View.OnClickListener() {
 //            @Override
 //            public void onClick(View view) {
-//                onViewClick(fpvWidget);
+//                onViewClick( fpvWidget );
 //            }
-//        });
+//        } );
 //        primaryVideoView = (RelativeLayout) findViewById(R.id.fpv_container);
 //        secondaryVideoView = (FrameLayout) findViewById(R.id.secondary_video_view);
 //        secondaryFPVWidget = findViewById(R.id.secondary_fpv_widget);
@@ -180,64 +179,46 @@ public class CompleteWidgetActivity extends Activity {
 //    private void onViewClick(View view) {
 //        if (view == fpvWidget && !isMapMini) {
 //            resizeFPVWidget(RelativeLayout.LayoutParams.MATCH_PARENT, RelativeLayout.LayoutParams.MATCH_PARENT, 0, 0);
-//            reorderCameraCapturePanel();
+////            reorderCameraCapturePanel();
 //            ResizeAnimation mapViewAnimation = new ResizeAnimation(mapWidget, deviceWidth, deviceHeight, width, height, margin);
 //            mapWidget.startAnimation(mapViewAnimation);
 //            isMapMini = true;
 //        } else if (view == mapWidget && isMapMini) {
 //            hidePanels();
 //            resizeFPVWidget(width, height, margin, 12);
-//            reorderCameraCapturePanel();
+////            reorderCameraCapturePanel();
 //            ResizeAnimation mapViewAnimation = new ResizeAnimation(mapWidget, width, height, deviceWidth, deviceHeight, 0);
 //            mapWidget.startAnimation(mapViewAnimation);
 //            isMapMini = false;
 //        }
 //    }
-
-    private void resizeFPVWidget(int width, int height, int margin, int fpvInsertPosition) {
-        RelativeLayout.LayoutParams fpvParams = (RelativeLayout.LayoutParams) primaryVideoView.getLayoutParams();
-        fpvParams.height = height;
-        fpvParams.width = width;
-        fpvParams.rightMargin = margin;
-        fpvParams.bottomMargin = margin;
-        if (isMapMini) {
-            fpvParams.addRule(RelativeLayout.CENTER_IN_PARENT, 0);
-            fpvParams.addRule(RelativeLayout.ALIGN_PARENT_RIGHT, RelativeLayout.TRUE);
-            fpvParams.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM, RelativeLayout.TRUE);
-        } else {
-            fpvParams.addRule(RelativeLayout.ALIGN_PARENT_RIGHT, 0);
-            fpvParams.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM, 0);
-            fpvParams.addRule(RelativeLayout.CENTER_IN_PARENT, RelativeLayout.TRUE);
-        }
-        primaryVideoView.setLayoutParams(fpvParams);
-
-        parentView.removeView(primaryVideoView);
-        parentView.addView(primaryVideoView, fpvInsertPosition);
-    }
+//
+//    private void resizeFPVWidget(int width, int height, int margin, int fpvInsertPosition) {
+//        RelativeLayout.LayoutParams fpvParams = (RelativeLayout.LayoutParams) primaryVideoView.getLayoutParams();
+//        fpvParams.height = height;
+//        fpvParams.width = width;
+//        fpvParams.rightMargin = margin;
+//        fpvParams.bottomMargin = margin;
+//        if (isMapMini) {
+//            fpvParams.addRule(RelativeLayout.CENTER_IN_PARENT, 0);
+//            fpvParams.addRule(RelativeLayout.ALIGN_PARENT_RIGHT, RelativeLayout.TRUE);
+//            fpvParams.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM, RelativeLayout.TRUE);
+//        } else {
+//            fpvParams.addRule(RelativeLayout.ALIGN_PARENT_RIGHT, 0);
+//            fpvParams.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM, 0);
+//            fpvParams.addRule(RelativeLayout.CENTER_IN_PARENT, RelativeLayout.TRUE);
+//        }
+//        primaryVideoView.setLayoutParams(fpvParams);
+//
+//        parentView.removeView(primaryVideoView);
+//        parentView.addView(primaryVideoView, fpvInsertPosition);
+//    }
 
 //    private void reorderCameraCapturePanel() {
 //        View cameraCapturePanel = findViewById(R.id.CameraCapturePanel);
 //        parentView.removeView(cameraCapturePanel);
 //        parentView.addView(cameraCapturePanel, isMapMini ? 9 : 13);
 //    }
-
-    private void swapVideoSource() {
-        if (secondaryFPVWidget.getVideoSource() == FPVWidget.VideoSource.SECONDARY) {
-            fpvWidget.setVideoSource(FPVWidget.VideoSource.SECONDARY);
-            secondaryFPVWidget.setVideoSource(FPVWidget.VideoSource.PRIMARY);
-        } else {
-            fpvWidget.setVideoSource(FPVWidget.VideoSource.PRIMARY);
-            secondaryFPVWidget.setVideoSource(FPVWidget.VideoSource.SECONDARY);
-        }
-    }
-
-    private void updateSecondaryVideoVisibility() {
-        if (secondaryFPVWidget.getVideoSource() == null) {
-            secondaryVideoView.setVisibility(View.GONE);
-        } else {
-            secondaryVideoView.setVisibility(View.VISIBLE);
-        }
-    }
 
     private void hidePanels() {
         //These panels appear based on keys from the drone itself.
